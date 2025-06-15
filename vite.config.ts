fc445b5d0c506fdae72c9717d8418bf7cec656ee
danblock97 <generalgoldiez@gmail.com>
2025-06-15 15:00:46 +0100

    refactor(dependencies): installed/set up all dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-	plugins: [react()],
-});
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+	server: {
+		host: "::",
+		port: 8080,
+	},
+	plugins: [
+		react(),
+		tailwindcss(),
+		mode === "development" && componentTagger(),
+	].filter(Boolean),
+	css: {
+		postcss: "./postcss.config.js",
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+}));
