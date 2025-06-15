eef58911e59acf88f9593789fa5e23dcf0166394
danblock97 <generalgoldiez@gmail.com>
2025-06-15 16:03:33 +0100

    feat(ui): add various UI components including Accordion, AlertDialog, Avatar, Badge, and more

---
+++ src/components/ui/sidebar.tsx
@@ -0,0 +1,36 @@
+export {
+	SidebarContext,
+	useSidebar,
+	SIDEBAR_COOKIE_NAME,
+	SIDEBAR_COOKIE_MAX_AGE,
+	SIDEBAR_WIDTH,
+	SIDEBAR_WIDTH_MOBILE,
+	SIDEBAR_WIDTH_ICON,
+	SIDEBAR_KEYBOARD_SHORTCUT,
+} from "./sidebar/context";
+export { SidebarProvider } from "./sidebar/SidebarProvider";
+export { Sidebar } from "./sidebar/Sidebar";
+export { SidebarTrigger } from "./sidebar/SidebarTrigger";
+export { SidebarRail } from "./sidebar/SidebarRail";
+export { SidebarInset } from "./sidebar/SidebarInset";
+export { SidebarInput } from "./sidebar/SidebarInput";
+export { SidebarHeader } from "./sidebar/SidebarHeader";
+export { SidebarFooter } from "./sidebar/SidebarFooter";
+export { SidebarSeparator } from "./sidebar/SidebarSeparator";
+export { SidebarContent } from "./sidebar/SidebarContent";
+export { SidebarGroup } from "./sidebar/SidebarGroup";
+export { SidebarGroupLabel } from "./sidebar/SidebarGroupLabel";
+export { SidebarGroupAction } from "./sidebar/SidebarGroupAction";
+export { SidebarGroupContent } from "./sidebar/SidebarGroupContent";
+export { SidebarMenu } from "./sidebar/SidebarMenu";
+export { SidebarMenuItem } from "./sidebar/SidebarMenuItem";
+export {
+	SidebarMenuButton,
+	sidebarMenuButtonVariants,
+} from "./sidebar/SidebarMenuButton";
+export { SidebarMenuAction } from "./sidebar/SidebarMenuAction";
+export { SidebarMenuBadge } from "./sidebar/SidebarMenuBadge";
+export { SidebarMenuSkeleton } from "./sidebar/SidebarMenuSkeleton";
+export { SidebarMenuSub } from "./sidebar/SidebarMenuSub";
+export { SidebarMenuSubItem } from "./sidebar/SidebarMenuSubItem";
+export { SidebarMenuSubButton } from "./sidebar/SidebarMenuSubButton";
