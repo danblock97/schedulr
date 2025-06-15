8cd1c46ce1fa9a42b4594f90d7bf22af98dbf365
danblock97 <generalgoldiez@gmail.com>
2025-06-15 18:11:50 +0100

    feat(analytics): integrate Vercel Analytics and Speed Insights, update page title and meta descriptions for improved SEO

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+		<SpeedInsights />
+		<Analytics />
+	</StrictMode>
+);
